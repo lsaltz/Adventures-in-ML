348efe9697ca934006c5d27268242e648b42e129
lsaltz <40012441+lsaltz@users.noreply.github.com>
2019-09-03 01:34:48 -0600

    Add files via upload

---
+++ GradientDescent.py
@@ -0,0 +1,15 @@
+xVal = 10
+rate = 0.2
+precision = 0.000000000000000000000001
+stepSize = 1
+maxIterations = 1000
+iteration = 0
+df = lambda x: 2*(x+5)
+print("Iteration 0 \nX value is: ",xVal)
+while stepSize > precision and iteration < maxIterations:
+    xPrev = xVal
+    xVal = xVal - rate * df(xPrev)
+    stepSize = abs(xVal-xPrev)
+    iteration = iteration + 1
+    print("Iteration ",iteration,"\nX value is: ", xVal)
+print("The local minimum occurs at", xVal)
